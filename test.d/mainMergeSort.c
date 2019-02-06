f433cb96967dfbc74b16d4c352356625a79d37aa
Dustin <duslxx@gmx.at>
2019-02-06 21:12:38 +0100

    all nice and clean

--- test.d/mainMergeSort.c
+++ test.d/mainMergeSort.c
@@ -5,10 +5,10 @@
 int main()
 {
     int i, N, k;	
-	N = 16;
+	N = 1024 << 20;
 	
-	int *arr = (int *)malloc(N*sizeof(int));
-	int* workArr = (int *)malloc(N*sizeof(int));	
+	int *arr = (int*) malloc(N*sizeof(int));
+	int* workArr = (int*) malloc(N*sizeof(int));	
 
     printf("\n");
 	for (i=0; i<N; i++)
