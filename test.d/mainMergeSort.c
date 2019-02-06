20cc59b2373329c9254741c4f310db2dd21ff6d6
Dustin <duslxx@gmx.at>
2019-02-06 21:26:10 +0100

    all nice and clean

--- test.d/mainMergeSort.c
+++ test.d/mainMergeSort.c
@@ -5,27 +5,25 @@
 int main()
 {
     int i, N, k;	
-	N = 2 << 30;
+	N = 8 << 26;
 	
 	int *arr = (int*) malloc(N*sizeof(int));
 	int* workArr = (int*) malloc(N*sizeof(int));	
 
-    printf("\n");
 	for (i=0; i<N; i++)
     {
 	    arr[i] = (int) (rand() % (2*N));
-	    printf("%d ", arr[i]);
+	    // printf("%d ", arr[i]);
     }
 
-    printf("\n");
 	mergesort(arr, workArr, N);
 
 	for(k=0; k<N; k++)
     {
 	    arr[k]=workArr[k];
-	    printf("%d ", workArr[k]);
+	    // printf("%d ", workArr[k]);
     }
 
-    printf("\n");
+    printf("\n\t%lu GB\n", sizeof(int) * N * 2 / 1024 / 1024 / 1024);
     return 0;
 }
